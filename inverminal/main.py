20ecdf470f69378cd44c96a7b34a6d61145f6856
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-27 23:50:50 +0500

    added startup script

--- inverminal/main.py
+++ inverminal/main.py
@@ -1,9 +1,13 @@
 def run():
     from youtubesearchpython import VideosSearch
     import youtube_dl, os, sys
-
+    search_query = ' '.join(sys.argv[1:]).strip()
+    if len(search_query) == 0:
+        print("No search query provided.")
+        exit()
     ydl = youtube_dl.YoutubeDL()
-    videosSearch = VideosSearch(' '.join(sys.argv[1:]), limit = 1)
+    print("Searching", search_query)
+    videosSearch = VideosSearch(search_query, limit = 1)
     result = videosSearch.result()['result'][0]
     info_dict = ydl.extract_info(result['id'], download=False)
     url = info_dict['formats'][0]['url']
