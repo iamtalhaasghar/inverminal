0ec351ce57dbbaf0f7b487bbd01322c8e02fef53
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-28 00:01:46 +0500

    fixed no module named "inverminal" issue

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup
 
 options = dict(
     name="inverminal",
-    version="1.0.1",
+    version="1.0.2",
     description="Use Invidious on Terminal to watch Youtube Videos or play just audio in background.",
     keywords=["video", "music", "audio", "youtube", "stream", "download", "inverminal"],
     author="talha_programmer",
     author_email="talhaasghar.contact@simplelogin.fr",
     url="https://github.com/iamtalhaasghar/inverminal",
     download_url="https://github.com/iamtalhaasghar/inverminal/releases",
+    packages=['inverminal'],
     entry_points={'console_scripts': ['inv = inverminal.main:run']},
     install_requires= open("requirements.txt").readlines(),
     long_description=open("README.md").read()
