d0bcd66ab99547e670cf5fd6b6032f864ccdb25d
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-01-28 00:59:43 +0500

    use subprocess to run vlc

--- inverminal/main.py
+++ inverminal/main.py
@@ -1,10 +1,10 @@
 from youtubesearchpython import VideosSearch
-import youtube_dl, os, sys
+import youtube_dl, os, subprocess
 
 
 def run():
     ydl = youtube_dl.YoutubeDL()
-    search_query = input("(q=quit) > ")
+    search_query = input("(search) > ")
     while( search_query != 'q'):
         if len(search_query) == 0:
             print("No search query provided.")
@@ -14,15 +14,17 @@ def run():
             results = videosSearch.result()['result']
             for i,j in enumerate(results):
                 print(i, j['title'])
-            selected_video_num = int(input("> "))
+            selected_video_num = int(input("(select) > "))
             result = results[selected_video_num]
             info_dict = ydl.extract_info(result['id'], download=False)
             url = info_dict['formats'][0]['url']
             print('Playing', result['title'])
-            os.system('/usr/bin/vlc "%s" --meta-title="%s"' % (url, result['title']))
+            with open(os.devnull, "w") as devnull:
+                subprocess.run(['vlc', url, '--meta-title=%s' % result['title']], shell=False, stderr=devnull)
+            #os.system('/usr/bin/vlc "%s" --meta-title="%s"' % (url, result['title']))
         except Exception as ex:
             print(ex)
-        search_query = input("(q=quit) > ")
+        search_query = input("(search) > ")
 
 if __name__ == '__main__':
     run()
